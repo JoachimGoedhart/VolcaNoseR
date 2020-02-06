dcff3d0d215a6d356450b30d1e52aa7cabf361e8
Joachim Goedhart <j.goedhart@uva.nl>
2020-02-06 17:00:19 +0100

    Added feature
    Added option to select the criterion for selecting and ranking hits

--- app.R
+++ app.R
@@ -67,17 +67,20 @@ ui <- fluidPage(
             sliderInput("alphaInput", "Visibility of the data", 0, 1, 0.8),  
 
             # hr(),
+            h4("Identification & Annotation of hits"),
             sliderInput("fc_cutoff", "Fold Change threshold:", 0, 5, step=0.1, value = 1.5),
             sliderInput("p_cutoff", "Significance threshold:", 0, 5, step=0.1, value = 2),
-            selectInput("direction", label="Use threshold to highlight:", choices = list("All"="all", "Only Significant"="significant","Only Increased"="increased", "Only Decreased"="decreased"), selected ="all"),
+            selectInput("direction", label="Use thresholds to annotate:", choices = list("All (ignores thresholds)"="all", "Changed (&significant)"="significant","Increased (&significant)"="increased", "Decreased (&significant)"="decreased"), selected ="all"),
             
+            selectInput("criterion", label="Criterion for ranking hits:", choices = list("Manhattan distance"="manh", "Euclidean distance"="euclid","Fold change"="fc","Significance"="sig"), selected ="manh"),
             
-            h4("Annotation of hits"),
+
             numericInput("top_x", "Number of top hits (0 to hide):", value = 10),
+            
             # textInput("user_gene_list2", "Selected hits (names separated by commas, e.g. DOK6,TBX5)", value = ""), 
             
             selectizeInput(inputId = 'user_gene_list',
-                           label = "Selected hits",
+                           label = "User selected hits",
                            choices = "-",
                           selected = "-",
                            multiple = TRUE, # allow for multiple inputs
@@ -535,7 +538,7 @@ observe({
     updateSliderInput(session, "fc_cutoff", value = presets_vis[3])
     updateSliderInput(session, "p_cutoff", value = presets_vis[4])
     updateSelectInput(session, "direction", selected = presets_vis[5])
-
+    updateSelectInput(session, "criterion", selected = presets_vis[6])
     
   }
   
@@ -660,7 +663,7 @@ url <- reactive({
   
   data <- c(input$data_input, "", input$x_var, input$y_var, input$g_var)
   
-  vis <- c(input$pointSize, input$alphaInput, input$fc_cutoff, input$p_cutoff, input$direction)
+  vis <- c(input$pointSize, input$alphaInput, input$fc_cutoff, input$p_cutoff, input$direction, input$criterion)
   
 
   #Convert the list of genes to a comma-seperated string  
@@ -753,10 +756,21 @@ observeEvent(input$settings_copy , {
           df <- df %>% filter(Change!="Unchanged")
           
         }
-              
+         
+        
+        if (input$criterion == "manh") {    
         df <- df %>% mutate(`Manhattan distance` = abs(`Significance`)+abs(`Fold change`)) %>% arrange(desc(`Manhattan distance`))
-      
         df_out <- df %>% top_n(input$top_x,`Manhattan distance`) %>% select(Name, Change, `Fold change`,`Significance`,`Manhattan distance`)
+        } else if (input$criterion == "euclid") {    
+          df <- df %>% mutate(`Euclidean distance` = sqrt((`Significance`)^2+(`Fold change`)^2)) %>% arrange(desc(`Euclidean distance`))
+          df_out <- df %>% top_n(input$top_x,`Euclidean distance`) %>% select(Name, Change, `Fold change`,`Significance`,`Euclidean distance`)
+        } else if (input$criterion == "fc") {    
+          df <- df %>% arrange(desc(abs(`Fold change`)))
+          df_out <- df %>% top_n(input$top_x,abs(`Fold change`)) %>% select(Name, Change, `Fold change`,`Significance`)
+        } else if (input$criterion == "sig") {    
+          df <- df %>% arrange(desc(`Significance`))
+          df_out <- df %>% top_n(input$top_x,`Significance`) %>% select(Name, Change, `Fold change`,`Significance`)
+        }
         
         #Add user selected hits, but remove them when already present
         df_out <- bind_rows(df_out,df_user()) %>% distinct(Name, .keep_all = TRUE)
